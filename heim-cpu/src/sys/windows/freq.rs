f5e394beaf3d4666447b25bd4a4c1974c6d348c2
apennamen <adrien.pennamen@gmail.com>
2021-01-29 00:00:12 +0100

    feat(wmi): introduce strategy

--- heim-cpu/src/sys/windows/freq.rs
+++ heim-cpu/src/sys/windows/freq.rs
@@ -11,64 +11,98 @@ use heim_common::units::{frequency, Frequency};
 use super::bindings::get_system_info;
 use super::bindings::power::PROCESSOR_POWER_INFORMATION;
 
-#[derive(Debug)]
-pub struct CpuFrequency(PROCESSOR_POWER_INFORMATION);
+trait FreqStrategy: std::fmt::Debug {
+    fn current(&self) -> Frequency;
+    fn max(&self) -> Option<Frequency>;
+    fn min(&self) -> Option<Frequency>;
+}
 
-impl CpuFrequency {
-    pub fn current(&self) -> Frequency {
+#[derive(Debug)]
+struct WinternlStrategy(PROCESSOR_POWER_INFORMATION);
+
+impl WinternlStrategy {
+    pub async fn new() -> Result<WinternlStrategy> {
+        let processors = get_processors()?;
+
+        processors
+            .into_iter()
+            .next()
+            .map(Self)
+            .ok_or_else(|| {
+                let inner = io::Error::from(io::ErrorKind::InvalidData);
+                Error::from(inner).with_message("No processors were found")
+            })
+    }
+
+    fn get_processors() -> Result<Vec<PROCESSOR_POWER_INFORMATION>> {
+        let info = get_system_info();
+        if info.dwNumberOfProcessors == 0 {
+            let inner = io::Error::from(io::ErrorKind::InvalidData);
+            return Err(Error::from(inner).with_message("No processors were found"));
+        }
+    
+        let proc_amount = info.dwNumberOfProcessors as usize;
+        let mut processors = Vec::<PROCESSOR_POWER_INFORMATION>::with_capacity(proc_amount);
+        let buffer_length = proc_amount * mem::size_of::<PROCESSOR_POWER_INFORMATION>();
+    
+        let result = unsafe {
+            powerbase::CallNtPowerInformation(
+                winnt::ProcessorInformation,
+                ptr::null_mut(),
+                0,
+                processors.as_mut_ptr() as *mut _,
+                buffer_length as minwindef::ULONG,
+            )
+        };
+    
+        if result == ntstatus::STATUS_SUCCESS {
+            unsafe {
+                processors.set_len(proc_amount);
+            }
+    
+            Ok(processors)
+        } else {
+            Err(Error::last_os_error().with_ffi("CallNtPowerInformation"))
+        }
+    }
+}
+
+impl FreqStrategy for WinternlStrategy { 
+    fn current(&self) -> Frequency {
         Frequency::new::<frequency::megahertz>(self.0.CurrentMhz.into())
     }
 
-    pub fn max(&self) -> Option<Frequency> {
+    fn max(&self) -> Option<Frequency> {
         Some(Frequency::new::<frequency::megahertz>(self.0.MaxMhz.into()))
     }
 
-    pub fn min(&self) -> Option<Frequency> {
+    fn min(&self) -> Option<Frequency> {
         None
     }
 }
 
-fn get_processors() -> Result<Vec<PROCESSOR_POWER_INFORMATION>> {
-    let info = get_system_info();
-    if info.dwNumberOfProcessors == 0 {
-        let inner = io::Error::from(io::ErrorKind::InvalidData);
-        return Err(Error::from(inner).with_message("No processors were found"));
+#[derive(Debug)]
+pub struct CpuFrequency {
+    // We use trait object to represent the abstract strategy
+    strategy: Box<dyn FreqStrategy>,
+}
+
+impl CpuFrequency {
+    pub fn current(&self) -> Frequency {
+        self.strategy.current()
     }
 
-    let proc_amount = info.dwNumberOfProcessors as usize;
-    let mut processors = Vec::<PROCESSOR_POWER_INFORMATION>::with_capacity(proc_amount);
-    let buffer_length = proc_amount * mem::size_of::<PROCESSOR_POWER_INFORMATION>();
+    pub fn max(&self) -> Option<Frequency> {
+        self.strategy.max()
+    }
 
-    let result = unsafe {
-        powerbase::CallNtPowerInformation(
-            winnt::ProcessorInformation,
-            ptr::null_mut(),
-            0,
-            processors.as_mut_ptr() as *mut _,
-            buffer_length as minwindef::ULONG,
-        )
-    };
-
-    if result == ntstatus::STATUS_SUCCESS {
-        unsafe {
-            processors.set_len(proc_amount);
-        }
-
-        Ok(processors)
-    } else {
-        Err(Error::last_os_error().with_ffi("CallNtPowerInformation"))
+    pub fn min(&self) -> Option<Frequency> {
+        self.strategy.min()
     }
 }
 
 pub async fn frequency() -> Result<CpuFrequency> {
-    let processors = get_processors()?;
+    let strategy = WinternlStrategy::new().await?;
 
-    processors
-        .into_iter()
-        .next()
-        .map(CpuFrequency)
-        .ok_or_else(|| {
-            let inner = io::Error::from(io::ErrorKind::InvalidData);
-            Error::from(inner).with_message("No processors were found")
-        })
+    Ok(CpuFrequency { strategy: Box::new(strategy) })
 }
