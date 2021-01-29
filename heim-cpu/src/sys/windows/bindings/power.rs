d2f3b6779de18f6ea4852e3ff6a9911cf9681ae0
apennamen <adrien.pennamen@gmail.com>
2021-01-29 20:29:53 +0100

    chore: refactor power aligned to winternl

--- heim-cpu/src/sys/windows/bindings/power.rs
+++ heim-cpu/src/sys/windows/bindings/power.rs
@@ -1,4 +1,15 @@
-use winapi::shared::minwindef;
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
+
+use std::io;
+use std::mem;
+use std::ptr;
+
+use winapi::shared::{minwindef, ntstatus};
+use winapi::um::{powerbase, winnt};
+
+use heim_common::prelude::{Error, Result};
+
+use super::get_system_info;
 
 #[repr(C)]
 #[derive(Debug)]
@@ -10,4 +21,38 @@ pub struct PROCESSOR_POWER_INFORMATION {
     pub MhzLimit: minwindef::ULONG,
     pub MaxIdleState: minwindef::ULONG,
     pub CurrentIdleState: minwindef::ULONG,
+}
+
+
+// Safe wrapper around the `CallNtPowerInformation`
+pub fn query_processor_information() -> Result<Vec<PROCESSOR_POWER_INFORMATION>> {
+    let info = get_system_info();
+    if info.dwNumberOfProcessors == 0 {
+        let inner = io::Error::from(io::ErrorKind::InvalidData);
+        return Err(Error::from(inner).with_message("No processors were found"));
+    }
+
+    let proc_amount = info.dwNumberOfProcessors as usize;
+    let mut processors = Vec::<PROCESSOR_POWER_INFORMATION>::with_capacity(proc_amount);
+    let buffer_length = proc_amount * mem::size_of::<PROCESSOR_POWER_INFORMATION>();
+
+    let result = unsafe {
+        powerbase::CallNtPowerInformation(
+            winnt::ProcessorInformation,
+            ptr::null_mut(),
+            0,
+            processors.as_mut_ptr() as *mut _,
+            buffer_length as minwindef::ULONG,
+        )
+    };
+
+    if result == ntstatus::STATUS_SUCCESS {
+        unsafe {
+            processors.set_len(proc_amount);
+        }
+
+        Ok(processors)
+    } else {
+        Err(Error::last_os_error().with_ffi("CallNtPowerInformation"))
+    }
 }
--- heim-cpu/src/sys/windows/freq.rs
+++ heim-cpu/src/sys/windows/freq.rs
@@ -1,15 +1,9 @@
 use std::io;
-use std::mem;
-use std::ptr;
-
-use winapi::shared::{minwindef, ntstatus};
-use winapi::um::{powerbase, winnt};
 
 use heim_common::prelude::*;
 use heim_common::units::{frequency, Frequency};
 
-use super::bindings::get_system_info;
-use super::bindings::power::PROCESSOR_POWER_INFORMATION;
+use super::bindings::power::{self, PROCESSOR_POWER_INFORMATION};
 
 trait FreqStrategy: std::fmt::Debug {
     fn current(&self) -> Frequency;
@@ -22,7 +16,7 @@ struct WinternlStrategy(PROCESSOR_POWER_INFORMATION);
 
 impl WinternlStrategy {
     pub async fn new() -> Result<WinternlStrategy> {
-        let processors = get_processors()?;
+        let processors = power::query_processor_information()?;
 
         processors
             .into_iter()
@@ -32,38 +26,6 @@ impl WinternlStrategy {
                 let inner = io::Error::from(io::ErrorKind::InvalidData);
                 Error::from(inner).with_message("No processors were found")
             })
-    }
-
-    fn get_processors() -> Result<Vec<PROCESSOR_POWER_INFORMATION>> {
-        let info = get_system_info();
-        if info.dwNumberOfProcessors == 0 {
-            let inner = io::Error::from(io::ErrorKind::InvalidData);
-            return Err(Error::from(inner).with_message("No processors were found"));
-        }
-    
-        let proc_amount = info.dwNumberOfProcessors as usize;
-        let mut processors = Vec::<PROCESSOR_POWER_INFORMATION>::with_capacity(proc_amount);
-        let buffer_length = proc_amount * mem::size_of::<PROCESSOR_POWER_INFORMATION>();
-    
-        let result = unsafe {
-            powerbase::CallNtPowerInformation(
-                winnt::ProcessorInformation,
-                ptr::null_mut(),
-                0,
-                processors.as_mut_ptr() as *mut _,
-                buffer_length as minwindef::ULONG,
-            )
-        };
-    
-        if result == ntstatus::STATUS_SUCCESS {
-            unsafe {
-                processors.set_len(proc_amount);
-            }
-    
-            Ok(processors)
-        } else {
-            Err(Error::last_os_error().with_ffi("CallNtPowerInformation"))
-        }
     }
 }
 
